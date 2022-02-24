0cfe45a91bb2a8ceb36552e7566910d8f88a1985
Keith <98364059+KeithMitchell9@users.noreply.github.com>
2022-02-24 05:43:18 +0000

    Completed the lab and the questions in the md file.

--- main.py
+++ main.py
@@ -8,96 +8,112 @@ import time
 ###
 
 def simple_work_calc(n, a, b):
-	"""Compute the value of the recurrence $W(n) = aW(n/b) + n
+  if n <= 1:
+    return n
+  else:
+    return a * simple_work_calc(n//b, a, b) + n
 
-	Params:
-	n......input integer
-	a......branching factor of recursion tree
-	b......input split factor
-
-	Returns: the value of W(n).
-	"""
-	# TODO
-	pass
 
 def test_simple_work():
-	""" done. """
-	assert work_calc(10, 2, 2) == #TODO
-	assert work_calc(20, 3, 2) == #TODO
-	assert work_calc(30, 4, 2) == #TODO
+  assert simple_work_calc(10, 2, 2) == 36
+  assert simple_work_calc(20, 3, 2) == 230
+  assert simple_work_calc(30, 4, 2) == 650
+  assert simple_work_calc(8, 2, 2) == 32
+  assert simple_work_calc(8, 3, 2) == 65
 
 def work_calc(n, a, b, f):
-	"""Compute the value of the recurrence $W(n) = aW(n/b) + f(n)
+  if n == 0:
+    return 0
+  elif n == 1:
+    return 1
+  else:
+    return a * work_calc(n//2, a, b, f) + f(n)
+  """Compute the value of the recurrence $W(n) = aW(n/b) + f(n)
 
-	Params:
-	n......input integer
-	a......branching factor of recursion tree
-	b......input split factor
-	f......a function that takes an integer and returns 
+  Params:
+  n......input integer
+  a......branching factor of recursion tree
+  b......input split factor
+  f......a function that takes an integer and returns 
            the work done at each node 
 
-	Returns: the value of W(n).
-	"""
-	# TODO
-	pass
+  Returns: the value of W(n).
+  """
 
 def span_calc(n, a, b, f):
-	"""Compute the span associated with the recurrence $W(n) = aW(n/b) + f(n)
+  if n == 0:
+    return 0
+  elif n == 1:
+    return 1
+  else:
+    return span_calc(n//2, a, b, f) + f(n)
+  """Compute the span associated with the recurrence $W(n) = aW(n/b) + f(n)
 
-	Params:
-	n......input integer
-	a......branching factor of recursion tree
-	b......input split factor
-	f......a function that takes an integer and returns 
+  Params:
+  n......input integer
+  a......branching factor of recursion tree
+  b......input split factor
+  f......a function that takes an integer and returns 
            the work done at each node 
 
-	Returns: the value of W(n).
-	"""
-	# TODO
-	pass
+  Returns: the value of W(n).
+  """
 
 def test_work():
+  assert work_calc(10, 2, 2, lambda n: 1) == 15
+  assert work_calc(20, 1, 2, lambda n: n*n) == 530
+  assert work_calc(30, 3, 2, lambda n: n) == 300
+  assert work_calc(8, 2, 2,lambda n: n) == 32 
+  assert work_calc(8, 1, 2, lambda n: n*n) == 85
+  assert work_calc(8, 3, 2, lambda n: 1) == 40
+
+def test_span():
 	""" done. """
-	assert work_calc(10, 2, 2,lambda n: 1) == #TODO
-	assert work_calc(20, 1, 2, lambda n: n*n) == #TODO
-	assert work_calc(30, 3, 2, lambda n: n) == #TODO
+	assert span_calc(10, 2, 2, lambda n: 1) == 4
+	assert span_calc(20, 1, 2, lambda n: n*n) == 530
+	assert span_calc(30, 3, 2, lambda n: n) == 56
 
 def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
-	"""
-	Compare the values of different recurrences for 
-	given input sizes.
+  
+  result = []
+  for n in sizes:
+    work_fn1 = lambda n: work_calc(n, 4, 2, lambda n: n)
+    work_fn2 = lambda n: work_calc(n, 4, 2, lambda n: n*n)
+    result.append((
+      n,
+      work_fn1(n),
+      work_fn2(n)
+      ))
+  print_results(result)
 
-	Returns:
-	A list of tuples of the form
-	(n, work_fn1(n), work_fn2(n), ...)
-	
-	"""
-	result = []
-	for n in input_sizes:
-		# compute W(n) using current a, b, f
-		result.append((
-			n,
-			work_fn1(n),
-			work_fn2(n)
-			))
-	return result
+def compare_span(span_fn1, span_fn2, sizes = [10, 20, 50, 100, 1000, 5000, 10000]):
+  result = []
+  for n in sizes:
+    span_fn1 = lambda n: span_calc(n, 4, 2, lambda n: n)
+    span_fn2 = lambda n: span_calc(n, 4, 2, lambda n: n*n)
+    result.append((
+      n,
+      span_fn1(n),
+      span_fn2(n)
+    ))
+  print_results(result)
 
 def print_results(results):
-	""" done """
 	print(tabulate.tabulate(results,
-							headers=['n', 'W_1', 'W_2'],
+							headers=['n', 'F_1', 'F_2'],
 							floatfmt=".3f",
 							tablefmt="github"))
 
 def test_compare_work():
-	# curry work_calc to create multiple work
-	# functions taht can be passed to compare_work
-    
-	# create work_fn1
-	# create work_fn2
+  work_fn1 = lambda n: work_calc(n, 4, 2, lambda n: n)
+  work_fn2 = lambda n: work_calc(n, 4, 2, lambda n: n*n)
 
-    res = compare_work(work_fn1, work_fn2)
-	print(res)
+  res = compare_work(work_fn1, work_fn2)
+  print(res)
 
 def test_compare_span():
-	# TODO
+  span_fn1 = lambda n: work_calc(n, 4, 2, lambda n: n)
+  span_fn2 = lambda n: work_calc(n, 4, 2, lambda n: n*n)
+
+  res = compare_work(span_fn1, span_fn2)
+  print(res)
